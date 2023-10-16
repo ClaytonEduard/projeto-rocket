70d761291abdc1fea2ad0602802406da83f83387
ClaytonEduard <clayton_eduard@hotmail.com>
2023-10-16 14:11:48 -0300

    Correcao das pgns do avatar

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains('light')) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', '/assets/avatar-light.png')
+    img.setAttribute('src', './assets/avatar-light.png')
     img.setAttribute('alt', 'Foto de Clayton Eduard sorrindo, usando camisa preta, barba e fundo branco.')
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', '/assets/avatar.png')
+    img.setAttribute('src', './assets/avatar.png')
     img.setAttribute('alt', 'Foto de Clayton Eduard sorrindo, usando camisa preta, com fundo da Avenida Paulista.')
   }
 }
